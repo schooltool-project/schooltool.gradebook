72e01e38f63e35e0b019660c7fd3ca0a392034dd
Alan Elkner <aelkner@gmail.com>
2011-06-28 18:59:11 -0400

    changed report request views to use radio buttons.

--- src/schooltool/gradebook/browser/request_reports.py
+++ src/schooltool/gradebook/browser/request_reports.py
@@ -238,24 +238,8 @@ class FlourishRequestStudentReportView(flourish.page.Page,
                                        RequestStudentReportView):
 
     def update(self):
-        if 'CANCEL' in self.request:
-            self.request.response.redirect(self.nextURL())
+        RequestStudentReportView.update(self)
 
     def form_title(self):
         return RequestStudentReportView.title(self)
-
-    def rows(self):
-        current_term = getUtility(IDateManager).current_term
-        current_year = ISchoolYear(current_term)
-        url = self.reportURL()
-        results = [{
-            'title': current_year.__name__,
-            'url': url,
-            }]
-        for term in current_year.values():
-            results.append({
-                'title': term.title,
-                'url': '%s?term=%s' % (url, term.__name__),
-                })
-        return results
 
