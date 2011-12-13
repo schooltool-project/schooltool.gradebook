94f6bf810a9b9b87a29f440973429c489e002ce3
Alan Elkner <aelkner@gmail.com>
2011-12-12 22:40:17 -0500

    created deployed_worksheets.html view for deploying course worksheet templates fixed message for empty tempates container

--- src/schooltool/gradebook/browser/course_worksheets.py
+++ src/schooltool/gradebook/browser/course_worksheets.py
@@ -24,6 +24,7 @@ from zope.container.interfaces import INameChooser
 from zope.browserpage.viewpagetemplatefile import ViewPageTemplateFile
 from zope.security.checker import canWrite
 from zope.security.interfaces import Unauthorized
+from zope.security.proxy import removeSecurityProxy
 from zope.traversing.api import getName
 from zope.traversing.browser.absoluteurl import absoluteURL
 from zope.i18n import translate
@@ -42,19 +43,25 @@ from schooltool.skin import flourish
 from schooltool.skin.flourish.page import TertiaryNavigationManager
 from schooltool.term.interfaces import ITerm
 
-from schooltool.gradebook.interfaces import IActivities, IWorksheet
+from schooltool.gradebook.interfaces import (IActivities, IWorksheet,
+    ICourseDeployedWorksheets)
 from schooltool.gradebook.activity import Worksheet, Activity
 
 
-class FlourishManageCourseSheetsOverview(flourish.page.Content):
-    """A flourish viewlet for showing deployed worksheets in course view"""
-
-    body_template = ViewPageTemplateFile(
-        'templates/f_manage_course_worksheets.pt')
+class FlourishCourseSchooYearMixin(object):
+    """A flourish mixin class for any course view with need of schooyear"""
 
     @property
     def schoolyear(self):
         return ISchoolYear(self.context)
+
+
+class FlourishManageCourseSheetsOverview(FlourishCourseSchooYearMixin,
+                                         flourish.page.Content):
+    """A flourish viewlet for showing deployed worksheets in course view"""
+
+    body_template = ViewPageTemplateFile(
+        'templates/f_manage_course_worksheets.pt')
 
     def sheets(self):
         return []
@@ -109,4 +116,153 @@ class FlourishCourseWorksheetAddView(flourish.form.AddForm):
         super(FlourishCourseWorksheetAddView, self).updateActions()
         self.actions['add'].addClass('button-ok')
         self.actions['cancel'].addClass('button-cancel')
+
+
+class FlourishCourseWorksheetsBase(FlourishCourseSchooYearMixin):
+
+    @property
+    def deployed(self):
+        return ICourseDeployedWorksheets(self.context)
+
+    @property
+    def activities(self):
+        return IActivities(self.context)
+
+    def sheets(self):
+        return [sheet for sheet in self.all_sheets() if not sheet['checked']]
+
+    def all_sheets(self):
+        schoolyear = self.schoolyear
+        deployments = {}
+        for sheet in self.deployed.values():
+            sheet = removeSecurityProxy(sheet)
+            index = int(sheet.__name__[sheet.__name__.rfind('_') + 1:])
+            deployment = deployments.setdefault(index, {
+                'obj': sheet,
+                'index': str(index),
+                'checked': sheet.hidden,
+                'terms': [False] * len(schoolyear),
+                })
+            for index, term in enumerate(schoolyear.values()):
+                if sheet.__name__.startswith(term.__name__):
+                    deployment['terms'][index] = True
+        sheets = [v for k, v in sorted(deployments.items())]
+        return ([sheet for sheet in sheets if not sheet['checked']] +
+                [sheet for sheet in sheets if sheet['checked']])
+
+
+class FlourishCourseWorksheetsView(FlourishCourseWorksheetsBase,
+                                   flourish.page.Page):
+    """A flourish view for managing corse worksheet deployment"""
+
+    def __init__(self, context, request):
+        super(FlourishCourseWorksheetsView, self).__init__(context, request)
+        self.alternate_title = self.request.get('alternate_title')
+
+    @property
+    def subtitle(self):
+        subtitle = _(u'Worksheets for ${year}',
+                  mapping={'year': self.schoolyear.title})
+        return translate(subtitle, context=self.request)
+
+    @property
+    def has_error(self):
+        return self.no_template or self.no_title
+
+    @property
+    def no_template(self):
+        return 'SUBMIT' in self.request and not self.request.get('template')
+
+    @property
+    def no_title(self):
+        return ('SUBMIT' in self.request and
+                not self.request.get('alternate_title'))
+
+    @property
+    def terms(self):
+        result = [{
+            'name': '',
+            'title': _('-- Entire year --'),
+            'selected': 'selected',
+            }]
+        for term in self.schoolyear.values():
+            result.append({
+                'name': term.__name__,
+                'title': term.title,
+                'selected': '',
+                })
+        return result
+
+    @property
+    def templates(self):
+        result = [{
+            'name': '',
+            'title': _('-- Select a template --'),
+            'selected': 'selected',
+            }]
+        for template in self.activities.values():
+            result.append({
+                'name': template.__name__,
+                'title': template.title,
+                'selected': '',
+                })
+        return result
+
+    def update(self):
+        if 'CANCEL' in self.request:
+            self.request.response.redirect(self.nextURL())
+        if 'SUBMIT' in self.request:
+            if self.request.get('template') and self.alternate_title:
+                template = self.activities[self.request['template']]
+                term = self.request.get('term')
+                if term:
+                    term = self.schoolyear[term]
+                self.deploy(term, template)
+                self.alternate_title = ''
+
+    def deploy(self, term, template):
+        # get the next index and title
+        highest, title_index = 0, 0
+        template_title = self.alternate_title
+        for sheet in self.deployed.values():
+            index = int(sheet.__name__[sheet.__name__.rfind('_') + 1:])
+            if index > highest:
+                highest = index
+            if sheet.title.startswith(template_title):
+                rest = sheet.title[len(template_title):]
+                if not rest:
+                    new_index = 1
+                elif len(rest) > 1 and rest[0] == '-' and rest[1:].isdigit():
+                    new_index = int(rest[1:])
+            else:
+                new_index = 0
+            if new_index > title_index:
+                title_index = new_index
+
+        # copy worksheet template to the term or whole year
+        if term:
+            terms = [term]
+        else:
+            terms = self.schoolyear.values()
+        for term in terms:
+            deployedKey = '%s_%s' % (term.__name__, highest + 1)
+            title = template_title
+            if title_index:
+                title += '-%s' % (title_index + 1)
+            deployedWorksheet = Worksheet(title)
+            self.deployed[deployedKey] = deployedWorksheet
+            continue
+            copyActivities(template, deployedWorksheet)
+
+            # now copy the template to all sections in the term
+            sections = ISectionContainer(term)
+            for section in sections.values():
+                worksheetCopy = Worksheet(deployedWorksheet.title)
+                worksheetCopy.deployed = True
+                self.activities[deployedWorksheet.__name__] = worksheetCopy
+                copyActivities(deployedWorksheet, worksheetCopy)
+
+    def nextURL(self):
+        url = absoluteURL(ISchoolToolApplication(None), self.request)
+        return url + '/manage'
 
